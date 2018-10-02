9ccca32aa8a0e9475a7bbb63d8f1463e6d21d54c
Tim Fischer <emailtimfischer@gmail.com>
2018-10-02 17:16:56 -0600

    Create function to generate random hex color

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,3 +6,13 @@ $('.generate-button').click(function () {
   event.preventDefault();
   console.log('hi');
 })
+
+function generateRandomColor() {
+  var letters = '0123456789ABCDEF'.split('');
+  var hexId = '#';
+
+  for (var i = 0; i < 6; i++) {
+    hexId += letters[Math.round(Math.random * 15)];
+  }
+  return hexId
+} 
