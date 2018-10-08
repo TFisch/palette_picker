fb6c7c2b2a844649be7605e1ee5ff58c67f9ad48
Tim Fischer <emailtimfischer@gmail.com>
2018-10-08 10:59:22 -0600

    Remove unused files

--- public/js/fetch.js
+++
@@ -1,7 +0,0 @@
-export const fetchProjects = async () => {
-  const url = "/api/v1/projects";
-  const response = await fetch(url);
-  const data = await response.json();
-  populateProjectMenu(data);
-  populateProjectList(data);
-}
